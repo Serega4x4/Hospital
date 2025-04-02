bdae23936e443ce5e338542db27bb7820cf81eea
Serega4x4 <serega1988bykov@gmail.com>
2025-04-02 09:01:41 +0200

    getout all logik from StoreAppointmentController to App\Services\Patient

--- app/Services/Patient/Service.php
+++ app/Services/Patient/Service.php
@@ -4,12 +4,78 @@ namespace App\Services\Patient;
 
 use App\Models\Appointment;
 use App\Models\Doctor;
+use Illuminate\Support\Facades\Auth;
 
 class Service
 {
-    public function store()
+    public function store($validated)
     {
-        //
+        $startTime = new \DateTime($validated['start_time']);
+        $doctor = Doctor::findOrFail($validated['doctor_id']);
+        $endTime = (clone $startTime)->modify("+{$doctor->appointment_duration} minutes");
+
+        // Checking doctor availability
+        $isBooked = Appointment::where('doctor_id', $doctor->id)
+            ->where(function ($query) use ($startTime, $endTime) {
+                $query
+                    ->whereBetween('start_time', [$startTime, $endTime])
+                    ->orWhereBetween('end_time', [$startTime, $endTime])
+                    ->orWhere(function ($query) use ($startTime, $endTime) {
+                        $query->where('start_time', '<=', $startTime)->where('end_time', '>=', $endTime);
+                    });
+            })
+            ->exists();
+
+        if ($isBooked) {
+            return redirect()
+                ->back()
+                ->withErrors(['start_time' => 'The doctor is busy at this time.'])
+                ->withInput();
+        }
+
+        // Checking doctor's working hours
+        $openingHours = $doctor->openingHours->first();
+        $hours = $openingHours ? $openingHours->hours : [];
+        $dayOfWeek = mb_strtolower($startTime->format('l'));
+
+        if (!isset($hours[$dayOfWeek])) {
+            return redirect()
+                ->back()
+                ->withErrors(['start_time' => 'The doctor does not work on this day.'])
+                ->withInput();
+        }
+
+        // Check time within working hours
+        $workingHours = $hours[$dayOfWeek];
+        $time = $startTime->format('H:i');
+        $isWithinHours = false;
+
+        foreach ($workingHours as $range) {
+            if ($range === null) {
+                continue;
+            }
+
+            [$open, $close] = explode('-', $range);
+            if ($time >= $open && $time < $close) {
+                $isWithinHours = true;
+                break;
+            }
+        }
+
+        if (!$isWithinHours) {
+            return redirect()
+                ->back()
+                ->withErrors(['start_time' => 'The doctor is not available at this time.'])
+                ->withInput();
+        }
+
+        $patient = Auth::user()->patient;
+        Appointment::create([
+            'start_time' => $startTime->format('Y-m-d H:i:s'),
+            'end_time' => $endTime->format('Y-m-d H:i:s'),
+            'patient_id' => $patient->id,
+            'doctor_id' => $doctor->id,
+        ]);
     }
 
     public function update()
